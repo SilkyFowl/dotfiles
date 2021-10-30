c908b223c6066d20df5fb78f537828ef4be56348
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-10-30 20:56:31 +0900

    Fix typo

--- .config/powershell/Microsoft.VSCode_profile.ps1
+++ .config/powershell/Microsoft.VSCode_profile.ps1
@@ -1,5 +1,3 @@
-. $home\Documents\PowerShell\Scripts\Invoke-Build.ArgumentCompleters.ps1
-
 Register-EditorCommand -Name PrintProcessID -DisplayName 'Print ProcessID' -ScriptBlock {
     "Current Process ID: $PID"
 }
